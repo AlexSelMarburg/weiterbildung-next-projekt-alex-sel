5269b3f23aee377b4730aae1f0d786e8bd1dd13f
Alex Sel <selalx2@aol.com>
2024-09-04 17:47:12 +0200

    deleted prev prisma migrations

--- prisma/migrations/20240904154419_initial_migration/migration.sql
+++ prisma/migrations/20240904154419_initial_migration/migration.sql
@@ -1,17 +1,17 @@
-/*
-  Warnings:
+-- CreateTable
+CREATE TABLE "User" (
+    "email" TEXT NOT NULL,
 
-  - The primary key for the `BookmarkedMovie` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - You are about to drop the column `id` on the `BookmarkedMovie` table. All the data in the column will be lost.
+    CONSTRAINT "User_pkey" PRIMARY KEY ("email")
+);
 
-*/
--- DropIndex
-DROP INDEX "BookmarkedMovie_movieID_userEmail_key";
+-- CreateTable
+CREATE TABLE "BookmarkedMovie" (
+    "movieID" INTEGER NOT NULL,
+    "userEmail" TEXT NOT NULL,
 
--- AlterTable
-ALTER TABLE "BookmarkedMovie" DROP CONSTRAINT "BookmarkedMovie_pkey",
-DROP COLUMN "id",
-ADD CONSTRAINT "BookmarkedMovie_pkey" PRIMARY KEY ("movieID");
+    CONSTRAINT "BookmarkedMovie_pkey" PRIMARY KEY ("movieID","userEmail")
+);
 
 -- CreateTable
 CREATE TABLE "Rating" (
@@ -23,6 +23,9 @@ CREATE TABLE "Rating" (
 
     CONSTRAINT "Rating_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
 ALTER TABLE "BookmarkedMovie" ADD CONSTRAINT "BookmarkedMovie_userEmail_fkey" FOREIGN KEY ("userEmail") REFERENCES "User"("email") ON DELETE RESTRICT ON UPDATE CASCADE;
